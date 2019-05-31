8567bc37e353e1c9237fa6dd74ba20aaecb6ca82
Srujan Bethi <ssbethi@gmail.com>
2019-05-31 13:54:21 -0600

    Configured http middleware proxy and routes

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,50 @@
 const express = require('express');
+const path = require('path');
+const proxy = require('http-proxy-middleware');
 const app = express();
-const bodyParser = require('body-parser');
+
+app.get('/', function(req, res) {
+  res.redirect(`/listings/${Math.floor(Math.random() * 99) + 1}`);
+});
 
 //Middleware
-console.log(__dirname);
 app.use(express.static(__dirname + '/../Public'));
-app.use(bodyParser.json());
+
+app.get('/listings/:id', function(req, res) {
+  const reactPath = path.join(__dirname, '../public/index.html');
+  res.sendFile(reactPath);
+});
+
+app.use('/listings/:id',
+  proxy({
+    target: 'http://127.0.0.1:3001'
+  })
+);
+
+app.use('/api/listings/:id/tour',
+  proxy({
+    target: 'http://127.0.0.1:3002'
+  })
+);
+
+app.use('/amenities/:id',
+  proxy({
+    target: 'http://127.0.0.1:3003'
+  })
+);
+
+app.use(
+  '/reviews/:id',
+  proxy({
+    target: 'http://127.0.0.1:3004'
+  })
+);
+
+app.use('/host/id/:id',
+  proxy({
+    target: 'http://127.0.0.1:3005'
+  })
+)
 
 //Start server
 const port = 3000;
